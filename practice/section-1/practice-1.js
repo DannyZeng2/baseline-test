60ffb800b2825a98b5978ad04238e88f95f15d3e
曾志威 <51374393+DannyZeng2@users.noreply.github.com>
2019-07-08 13:25:56 +0800

    Update practice-1.js

--- practice/section-1/practice-1.js
+++ practice/section-1/practice-1.js
@@ -2,10 +2,10 @@
 
 function collectSameElements(collectionA, collectionB) {
   const collectionC = [];
-  for(var i= 0;i<collectionA.length;i++){
-    for(var j= 0;j<collectionB.length;j++){
-      if(collectionA[i] == collectionB[j]) {
-        collectionC.push(collectionA[i]);
+  for(let valA of collectionA){
+    for(let valB of collectionB){
+      if(valA == valB) {
+        collectionC.push(valA);
       }
     }
   }
