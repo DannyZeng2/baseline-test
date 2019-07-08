824abdb5d2b938c4a44594c73c6930657e982ebf
曾志威 <51374393+DannyZeng2@users.noreply.github.com>
2019-07-08 11:48:05 +0800

    Update practice-4.js

--- practice/section-1/practice-4.js
+++ practice/section-1/practice-4.js
@@ -1,5 +1,15 @@
 'use strict';
 
 function collectSameElements(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  const collectionC = [];
+  const collectionD = objectB.value;
+  for(var i= 0;i<collectionA.length;i++){
+    for(var j= 0;j<collectionD.length;j++){
+      if(collectionA[i].key == collectionD[j]) {
+        collectionC.push(collectionA[i].key);
+      }
+    }
+  }
+
+  return collectionC;
 }
